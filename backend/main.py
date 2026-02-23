24c9af794a764ed840274b834e2e7cbf00b57cbe
mangofish369 <todologd@gmail.com>
2026-02-23 05:06:16 -0500

    fix failed to fetch bug, update requirement.txt

--- backend/main.py
+++ backend/main.py
@@ -17,7 +17,7 @@ def read_root():
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000" , "http://localhost:3001", "http://localhost:3002" ],
     allow_methods=["*"],
     allow_headers=["*"],
 )
